031bc2e8ac2751794f903fbb06dd13e38a2ee3b9
Ritesh Kishor Chaudhari <100477678+Ritu0097@users.noreply.github.com>
2024-02-05 12:09:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 function daysOfAYear(year) {
-	if(years%4==0){
-		return 366;
-	}
-	else{
-		return 365;
-	}
+    if (year % 4 === 0) {
+        if (year % 100 === 0) {
+            if (year % 400 === 0) {
+                return 366;
+            } else {
+                return 365;
+            }
+        } else {
+            return 366;
+        }
+    } else {
+        return 365;
+    }
 }
-daysOfAYear();
